3bb6a8828d1bebfc898469a6c37f944ee3a7b7d8
Andréa Leite <andrealeite.pf@gmail.com>
2025-05-16 14:31:02 -0300

    Resolução exercícios 3,4,6,7 e 8.

--- src/implementacao/8-multiplos-de-2-e-5.ts
+++ src/implementacao/8-multiplos-de-2-e-5.ts
@@ -1,39 +1,3 @@
-/*function analiseMultiplos(numeros: number[]) {
-    let multiplosDe2e5: number[] = [];
-    let multiplosDe2e3: number[] = [];
-    
-    for (const numero of numeros) {
-        if (numero === -1) break;
-        
-        if (numero % 2 === 0 && numero % 5 === 0) {
-            multiplosDe2e5.push(numero);
-        }
-
-        if (numero % 2 === 0 && numero % 3 === 0) {
-            multiplosDe2e3.push(numero);
-        }
-    }
-    
-    return {
-        multiplosDe2e5,
-        multiplosDe2e3
-    };
-}
-
-const listasDeValores = [
-    [10, 15, 30, -1],
-    [6, 12, 20, 25, -1],
-    [15, 30, -1]
-];
-
-for (const valores of listasDeValores) {
-    const { multiplosDe2e5, multiplosDe2e3 } = analiseMultiplos(valores);
-    console.log(`Para a lista ${valores}:`);
-    console.log(`a) Múltiplos de 2 e 5: ${multiplosDe2e5}`);
-    console.log(`b) Múltiplos de 2 e 3: ${multiplosDe2e3}`);
-}
-*/
-
 function analiseMultiplos(numeros: number[]) {
     let multiplosDe2e5 = 0;
     let multiplosDe2e3 = 0;
